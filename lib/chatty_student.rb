84908f8c56022707c1bb86c98a41934a6d233abc
Stela Capsa <stela.capsa@gmail.com>
2020-01-12 19:48:08 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,13 +2,14 @@ class ChattyStudent < Student
  
  
  def hello
-  super
-    p "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
-  
+
   def raise_hand
-   10.times do 
-     super 
-   end 
- end 
-end 
+    10.times do
+      super
+    end
+  end
+
+end
